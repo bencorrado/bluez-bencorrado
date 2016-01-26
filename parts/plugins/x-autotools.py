1b0f1e82cef5114578ba8c71dc8bf6cbf2a3bd8b
Tony Espy <espy@canonical.com>
2016-01-25 19:26:04 -0500

    Add custom autotools plugin which supports quilt

---
+++ parts/plugins/x-autotools.py
@@ -0,0 +1,90 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright (C) 2015 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""The autotools plugin is used for autotools based parts.
+
+Autotools based projects are the ones that have the usual
+`./configure && make && make install` instruction set.
+
+The plugin tries to build using ./configure first, if it is not there
+it will run ./autogen and if autogen is not there it will run autoreconf.
+
+This plugin uses the common plugin keywords as well as those for "sources".
+For more information check the 'plugins' topic for the former and the
+'sources' topic for the latter.
+
+In additon, this plugin uses the following plugin-specific keywords:
+
+    - configflags:
+      (list of strings)
+      configure flags to pass to the build such as those shown by running
+      './configure --help'
+"""
+
+import os
+import stat
+
+import snapcraft
+
+
+class AutotoolsPlugin(snapcraft.BasePlugin):
+
+    @classmethod
+    def schema(cls):
+        schema = super().schema()
+        schema['properties']['configflags'] = {
+            'type': 'array',
+            'minitems': 1,
+            'uniqueItems': True,
+            'items': {
+                'type': 'string',
+            },
+            'default': [],
+        }
+
+        return schema
+
+    def __init__(self, name, options):
+        super().__init__(name, options)
+        self.build_packages.extend([
+            'autoconf',
+            'automake',
+            'autopoint',
+            'libtool',
+            'make',
+        ])
+
+    def build(self):
+        super().build()
+        if not os.path.exists(os.path.join(self.builddir, "configure")):
+            autogen_path = os.path.join(self.builddir, "autogen.sh")
+            if os.path.exists(autogen_path):
+                # Make sure it's executable
+                if not os.access(autogen_path, os.X_OK):
+                    os.chmod(autogen_path,
+                             stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR |
+                             stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP |
+                             stat.S_IROTH | stat.S_IWOTH | stat.S_IXOTH)
+
+                self.run(['env', 'NOCONFIGURE=1', './autogen.sh'])
+            else:
+                self.run(['autoreconf', '-i'])
+        self.run(['pwd'])
+        os.environ['QUILT_PATCHES'] = '../../../patches'
+        self.run(['quilt', 'push', '-a'])
+        self.run(['./configure', '--prefix='] + self.options.configflags)
+        self.run(['make', 'install', 'DESTDIR=' + self.installdir])
+        self.run(['make', 'install', 'DESTDIR=' + self.installdir])
