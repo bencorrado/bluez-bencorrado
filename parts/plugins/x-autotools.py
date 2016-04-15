2b98ebd6413d8e352d2d32d92fa09ffb72a2dccd
Scott Sweeny <scott.sweeny@canonical.com>
2016-04-15 15:37:54 -0400

    Update local autotools plugin to match snapcraft changes

--- parts/plugins/x-autotools.py
+++ parts/plugins/x-autotools.py
@@ -72,8 +72,8 @@ class AutotoolsPlugin(snapcraft.BasePlugin):
 
         return schema
 
-    def __init__(self, name, options):
-        super().__init__(name, options)
+    def __init__(self, name, options, project):
+        super().__init__(name, options, project)
         self.build_packages.extend([
             'autoconf',
             'automake',
@@ -126,5 +126,5 @@ class AutotoolsPlugin(snapcraft.BasePlugin):
 
         self.run(configure_command + self.options.configflags)
         self.run(['make', '-j{}'.format(
-            snapcraft.common.get_parallel_build_count())])
+            self.project.parallel_build_count)])
         self.run(make_install_command)
