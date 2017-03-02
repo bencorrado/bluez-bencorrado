110cca10a044849cc9b04a0c0346080056d76724
Simon Fels <simon.fels@canonical.com>
2017-03-02 11:51:29 +0100

    Cleanup spread test setup to be inline with other components

--- tests/lib/prepare-all.sh
+++ tests/lib/prepare-all.sh
@@ -1,25 +1,36 @@
-#!/bin/bash
+#!/bin/sh
 
-# We don't have to build a snap when we should use one from a
-# channel
+# We don't have to build a snap when we should use one from a channel
 if [ -n "$SNAP_CHANNEL" ] ; then
 	exit 0
 fi
 
-# If there is a network-manager snap prebuilt for us, lets take
-# that one to speed things up.
-if [ -e /home/bluez/bluez_*_amd64.snap ] ; then
+# If there is a snap prebuilt for us, lets take that one to speed things up
+if [ -e $PROJECT_PATH/${SNAP_NAME}_*_${SNAP_ARCH}.snap ] ; then
 	exit 0
 fi
 
-# Setup classic snap and build the bluez snap in there
+
+# Setup classic snap and build the snap in there
 snap install --devmode --beta classic
 cat <<-EOF > /home/test/build-snap.sh
 #!/bin/sh
 set -ex
+
+export DEBIAN_FRONTEND=noninteractive
+
+# FIXME: Enable propose for now until problems with conflicting systemd
+# packages between the Ubuntu Core image ppa and the archive are fixed.
+echo "deb http://archive.ubuntu.com/ubuntu/ xenial-proposed restricted main universe" > /etc/apt/sources.list.d/ubuntu-proposed.list
+echo "deb http://ppa.launchpad.net/snappy-dev/image/ubuntu xenial main" > /etc/apt/sources.list.d/ubuntu-image-ppa.list
+
+# Ensure we have the latest updates installed as the core snap
+# may be a bit out of date.
 apt update
+apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' full-upgrade -y --force-yes
+
 apt install -y --force-yes snapcraft
-cd /home/bluez
+cd $PROJECT_PATH
 snapcraft clean
 snapcraft
 EOF
@@ -28,4 +39,4 @@ sudo classic /home/test/build-snap.sh
 snap remove classic
 
 # Make sure we have a snap build
-test -e /home/bluez/bluez_*_amd64.snap
+test -e $PROJECT_PATH/${SNAP_NAME}_*_${SNAP_ARCH}.snap
